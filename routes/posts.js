885bc46faaaa295c72d5537653999eb13cfce8cf
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-07 11:20:42 -0500

    add endpoint to update a posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,18 +3,27 @@ const router = express.Router();
 const pool = require("../db");
 
 router.get('/', (req, res) => {
-  pool.query("SELECT * FROM posts ORDER BY data_created DESC", (err, resp) => {
-      res.json(resp.rows);
-  });
+    pool.query("SELECT * FROM posts ORDER BY data_created DESC", (err, resp) => {
+        res.json(resp.rows);
+    });
 });
 
 router.post('/', (req, res, next) => {
     const { uid, title, body, username } = req.body;
-    pool.query(` *INSERT INTO posts(title, body, user_id, author, date_created)
+    pool.query(`INSERT INTO posts(title, body, user_id, author, date_created)
         VALUES(${title}, ${body}, ${uid}, ${username}, NOW())`, [], (err, resp) => {
             if (err) return next(err);
             res.json(resp.rows);
     });
-  });
-  
+});
+
+router.put('/', (req, res, next) => {
+    const { uid, pid, title, body, username } = req.body;
+    pool.query(`UPDATE posts SET title=${title}, body=${body}, user_id=${uid}, author=${username}, date_created=NOW())
+        WHERE pid=${pid}`, [], (err, resp) => {
+            if (err) return next(err);
+            res.json(resp.rows);
+    });
+});
+
 module.exports = router;
