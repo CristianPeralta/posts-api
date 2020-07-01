4d5c6c6ab637a7c1d52003d4ee3b83eb1d753f9b
CristianPeralta <cbperaltasegura@gmail.com>
2020-07-01 11:49:28 -0500

    config cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import cors, { CorsOptions } from 'cors';
 
 const app: Application = express();
+
+const allowedOrigins: Array<String> = [
+    'http://localhost:3000',
+];
+
+const options: CorsOptions = {
+  origin: (requestOrigin, callback) => {
+    if (!requestOrigin) return callback(null, true);
+    if (allowedOrigins.indexOf(requestOrigin) === -1) {
+      const msg: string = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+
+app.use(cors(options));
 
 const getGreeting = (name: string): string => `Hello ${name}`;
 
