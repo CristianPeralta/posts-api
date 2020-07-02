ce15101a0578e4c4fed2a0c5e61c28dc48cd387d
CristianPeralta <cbperaltasegura@gmail.com>
2020-07-02 15:17:51 -0500

    add request functions

--- src/tests/users.spec.ts
+++ src/tests/users.spec.ts
@@ -3,8 +3,8 @@ import app from '../app';
 
 describe('Get a user', () => {
     it('succeeds with correct credentials', async () => {
-        const query = '?username=tes1111';
-        const httpRequest = request(app).get(`/users${query}`);
+        const query = { username: 'tes1111' };
+        const httpRequest = get(`/users`, query);
         httpRequest.set('Accept', 'application/json')
         httpRequest.set('Origin', 'http://localhost:3000')
         const response = await httpRequest;
@@ -12,3 +12,19 @@ describe('Get a user', () => {
         expect(typeof response.body).toBe('object');
     });
 });
+
+export function post(url: string, body: object){
+    const httpRequest = request(app).post(url);
+    httpRequest.send(body);
+    httpRequest.set('Accept', 'application/json')
+    httpRequest.set('Origin', 'http://localhost:3000')
+    return httpRequest;
+}
+
+export function get(url: string, query: object){
+    const httpRequest = request(app).get(url);
+    httpRequest.query(query);
+    httpRequest.set('Accept', 'application/json')
+    httpRequest.set('Origin', 'http://localhost:3000')
+    return httpRequest;
+}
