4b603afb7e62ac3bb366b6e16d4136a146e509df
CristianPeralta <cbperaltasegura@gmail.com>
2020-07-07 16:45:17 -0500

    add test'Search posts'

--- src/tests/users.spec.ts
+++ src/tests/users.spec.ts
@@ -173,7 +173,6 @@ describe('Get posts', () => {
             expect(response.body[0]).toHaveProperty('pid');
             expect(response.body[0]).toHaveProperty('title');
             expect(response.body[0]).toHaveProperty('body');
-            expect(response.body[0].body.toLowerCase()).toEqual(expect.stringContaining('first'));
             expect(response.body[0]).toHaveProperty('user_id');
             expect(response.body[0]).toHaveProperty('author', query.username);
             expect(response.body[0]).toHaveProperty('like_user_id');
@@ -195,9 +194,30 @@ describe('Get posts', () => {
             expect(response.body[0]).toHaveProperty('pid');
             expect(response.body[0]).toHaveProperty('title');
             expect(response.body[0]).toHaveProperty('body');
-            expect(response.body[0].body.toLowerCase()).toEqual(expect.stringContaining('first'));
             expect(response.body[0]).toHaveProperty('user_id');
             expect(response.body[0]).toHaveProperty('author', query.userId);
+            expect(response.body[0]).toHaveProperty('like_user_id');
+            expect(response.body[0]).toHaveProperty('likes');
+            expect(response.body[0]).toHaveProperty('date_created');
+            expect(response.body[0]).toHaveProperty('search_vector');
+        }
+    });
+
+    it('Search posts', async () => {
+        const query = {
+            query: 'first',
+        };
+        const response = await get(`/posts/search`, query);
+        expect(response.status).toEqual(200);
+        expect(typeof response.body).toBe('object');
+        expect(Array.isArray(response.body)).toEqual(true);
+        if (response.body.length) {
+            expect(response.body[0]).toHaveProperty('pid');
+            expect(response.body[0]).toHaveProperty('title');
+            expect(response.body[0]).toHaveProperty('body');
+            expect(response.body[0].body.toLowerCase()).toEqual(expect.stringContaining('first'));
+            expect(response.body[0]).toHaveProperty('user_id');
+            expect(response.body[0]).toHaveProperty('author');
             expect(response.body[0]).toHaveProperty('like_user_id');
             expect(response.body[0]).toHaveProperty('likes');
             expect(response.body[0]).toHaveProperty('date_created');
